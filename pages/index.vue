feed6a5cbe3aae4719f43ef5a0a8ca9616708280
omni-frontend-dev <spadilla@omnislash.com>
2024-08-31 12:04:55 -0700

    landing page improvement

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,21 @@
 <template>
-  <h1>Landing Page</h1>
+  <div style="text-align: center; padding: 40px 0;">
+    <h1>Landing Page</h1>
+    
+    Inventory Systems can be complex. <br>
+    Lets make it simple. <br><br><br>
+    <div>
+      <el-link :underline="false" href="/login" type="primary">
+        Login
+      </el-link>
 
-  Are Inventory Systems too complex
-  Why can't Inventory systems be simple
+      <br><br><br>
 
-  
+      <el-link :underline="false" href="/register" type="primary">
+      Register
+      </el-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
