0f33fb84747ef998e8ac5bf8cec4bc932fe63ad1
omni-frontend-dev <spadilla@omnislash.com>
2024-10-15 22:37:03 -0700

    testing service workers

--- public/sw.js
+++ public/sw.js
@@ -1,49 +1,46 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js')
 
-// workbox.routing.registerRoute(
-//   /\.(?:css|js|html)$/,
-//   new workbox.strategies.StaleWhileRevalidate({
-//     cacheName: 'static',
-//   })
-// );
-
-// workbox.routing.registerRoute(
-//   ({ request }) => {
-//     const url = new URL(request.url)
-//     if(request.destination === 'fetch')
-//         console.log(url)
-//     const isHtmlCssJs =
-//       request.destination === 'document' ||
-//       request.destination === 'script' ||
-//       request.destination === 'style'
-
-//     return isHtmlCssJs;
-//   },
-//   new workbox.strategies.StaleWhileRevalidate({
-//     cacheName: 'static',
-//   })
-// );
-
-
 workbox.routing.registerRoute(
   ({ request }) => {
-    const url = new URL(request.url);
-    
     // Log the current request URL for debugging
-    console.log(`Request Destination: ${request.destination}, URL: ${url}`);
+    // const url = new URL(request.url)
+    // console.log(`Request Destination: ${request.destination}, URL: ${url}`)
     
     // Define which types of requests to cache
     const isCachedResource =
       request.destination === 'document' || // HTML documents
       request.destination === 'script' ||   // JS files
-      request.destination === 'style' ||    // CSS files
-      (request.destination === '' && (url.pathname.endsWith('.js') ||   // JS fetch requests
-                                       url.pathname.endsWith('.css') ||  // CSS fetch requests
-                                       url.pathname.endsWith('.html'))); // HTML fetch requests
+      request.destination === 'style'       // CSS files
 
-    return isCachedResource;
+    return isCachedResource
   },
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: 'static',
   })
-);
+)
+
+// Cache strategy for images
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === 'image',
+  new workbox.strategies.CacheFirst({
+    cacheName: 'image',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 1 * 24 * 60 * 60, // Cache for 1 Day (in seconds)
+      }),
+    ],
+  })
+)
+
+// Cache strategy for font
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === 'font',
+  new workbox.strategies.CacheFirst({
+    cacheName: 'font',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 30 * 24 * 60 * 60, // Cache for 30 Days
+      }),
+    ],
+  })
+)
