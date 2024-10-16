6933b314f222c33e95b3dba4b911933adbc0dc2f
omni-frontend-dev <spadilla@omnislash.com>
2024-10-15 20:07:30 -0700

    testing service workers

--- plugins/register-service-worker.client.js
+++ plugins/register-service-worker.client.js
@@ -1,15 +1,16 @@
 export default defineNuxtPlugin(() => {
-  if ('serviceWorker' in navigator) {
+  console.log(process.env.NODE_ENV)
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker.register('/sw.js')
-        .then(registration => {
-          console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch(error => {
-          console.error('Service Worker registration failed:', error);
-        });
-    });
+      .then(registration => {
+        console.log('Service Worker registered with scope:', registration.scope)
+      })
+      .catch(error => {
+        console.error('Service Worker registration failed:', error)
+      })
+    })
   } else {
-    console.log('Service Worker is not supported in this browser.');
+    console.log('Service Worker is not supported in this browser.')
   }
-});
+})
