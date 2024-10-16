2d9007286c831d78bfa971834e536b77341c8d62
omni-frontend-dev <spadilla@omnislash.com>
2024-10-15 22:10:54 -0700

    testing service workers

--- public/sw.js
+++ public/sw.js
@@ -7,18 +7,41 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox
 //   })
 // );
 
+// workbox.routing.registerRoute(
+//   ({ request }) => {
+//     const url = new URL(request.url)
+//     if(request.destination === 'fetch')
+//         console.log(url)
+//     const isHtmlCssJs =
+//       request.destination === 'document' ||
+//       request.destination === 'script' ||
+//       request.destination === 'style'
+
+//     return isHtmlCssJs;
+//   },
+//   new workbox.strategies.StaleWhileRevalidate({
+//     cacheName: 'static',
+//   })
+// );
+
+
 workbox.routing.registerRoute(
   ({ request }) => {
-    const url = new URL(request.url)
-    if(request.destination === 'fetch')
-        console.log(url)
-    const isHtmlCssJs =
-      request.destination === 'document' ||
-      request.destination === 'script' ||
-      request.destination === 'style' ||
-      request.destination === 'fetch'
+    const url = new URL(request.url);
+    
+    // Log the current request URL for debugging
+    console.log(`Request Destination: ${request.destination}, URL: ${url}`);
+    
+    // Define which types of requests to cache
+    const isCachedResource =
+      request.destination === 'document' || // HTML documents
+      request.destination === 'script' ||   // JS files
+      request.destination === 'style' ||    // CSS files
+      (request.destination === '' && (url.pathname.endsWith('.js') ||   // JS fetch requests
+                                       url.pathname.endsWith('.css') ||  // CSS fetch requests
+                                       url.pathname.endsWith('.html'))); // HTML fetch requests
 
-    return isHtmlCssJs;
+    return isCachedResource;
   },
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: 'static',
