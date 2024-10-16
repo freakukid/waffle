150d38d08dd3329e9e3a119da8e6347f09c8e662
omni-frontend-dev <spadilla@omnislash.com>
2024-10-15 20:26:12 -0700

    testing service workers

--- public/sw.js
+++ public/sw.js
@@ -1,15 +1,20 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js')
 
 workbox.routing.registerRoute(
-  ({ request }) => 
-    request.destination === 'script' ||
-    request.destination === 'style' ||
-    request.destination === 'document' ||
-    (request.destination === 'fetch' && 
-      (request.url.endsWith('.js') || 
-       request.url.endsWith('.css') || 
-       request.url.endsWith('.html'))),
+  ({ request }) => {
+    const url = new URL(request.url);
+    const isHtmlCssJs =
+      request.destination === 'document' ||
+      request.destination === 'script' ||
+      request.destination === 'style' ||
+      (request.destination === 'fetch' &&
+        (url.pathname.match(/\.js$/) || 
+         url.pathname.match(/\.css$/) || 
+         url.pathname.match(/\.html$/)));
+
+    return isHtmlCssJs;
+  },
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: 'static',
   })
-)
+);
