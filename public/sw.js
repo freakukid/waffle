69de4f60b2c521ad86a2d9642e6563328922f9dd
omni-frontend-dev <spadilla@omnislash.com>
2024-10-15 22:04:34 -0700

    testing service workers

--- public/sw.js
+++ public/sw.js
@@ -1,29 +1,26 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js')
 
-workbox.routing.registerRoute(
-  /\.(?:css|js|html)$/,
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'static',
-  })
-);
-
 // workbox.routing.registerRoute(
-//   ({ request }) => {
-//     const url = new URL(request.url)
-//     if(request.destination === 'fetch')
-//         console.log(url)
-//     const isHtmlCssJs =
-//       request.destination === 'document' ||
-//       request.destination === 'script' ||
-//       request.destination === 'style' ||
-//       (request.destination === 'fetch' &&
-//         (url.pathname.match(/\.js$/) || 
-//          url.pathname.match(/\.css$/) || 
-//          url.pathname.match(/\.html$/)));
-
-//     return isHtmlCssJs;
-//   },
+//   /\.(?:css|js|html)$/,
 //   new workbox.strategies.StaleWhileRevalidate({
 //     cacheName: 'static',
 //   })
 // );
+
+workbox.routing.registerRoute(
+  ({ request }) => {
+    const url = new URL(request.url)
+    if(request.destination === 'fetch')
+        console.log(url)
+    const isHtmlCssJs =
+      request.destination === 'document' ||
+      request.destination === 'script' ||
+      request.destination === 'style' ||
+      request.destination === 'fetch'
+
+    return isHtmlCssJs;
+  },
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'static',
+  })
+);
