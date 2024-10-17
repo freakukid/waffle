a8f7a9e704a5c5ac82e367cf041d5e674d42d431
omni-frontend-dev <spadilla@omnislash.com>
2024-10-16 23:01:54 -0700

    testing service workers

--- public/sw.js
+++ public/sw.js
@@ -63,73 +63,48 @@ workbox.routing.registerRoute(
 )
 
 const getAPIs = [
+  //Store
   'https://legitski.com/api/protected/store/stores', // String URL
-  /^https:\/\/legitski\.com\/api\/protected\/store\/\d+$/ // Regex pattern for individual store details
+  /^https:\/\/legitski\.com\/api\/protected\/store\/\d+$/, // Regex pattern for individual store details
+  //Log
+  /^https:\/\/legitski\.com\/api\/protected\/log\/\d+$/,
+  //Transaction
+  /^https:\/\/legitski\.com\/api\/protected\/transaction\/\d+$/,
+  //Layaway
+  /^https:\/\/legitski\.com\/api\/protected\/layaway\/\d+$/,
+  //Customer
+  /^https:\/\/legitski\.com\/api\/protected\/customer\/\d+$/,
+  //Workers
+  /^https:\/\/legitski\.com\/api\/protected\/workers\/\d+$/,
+  //Settings
+  'https://legitski.com/api/protected/settings/user-settings',
+  //Worker Permissions
+  /^https:\/\/legitski\.com\/api\/protected\/workers\/permissions\/\d+$/,
 ]
-
-function matchesApi(url) {
-  return getAPIs.some(api => {
-    if (typeof api === 'string') {
-      return api === url.href; // Check for string match
-    } else if (api instanceof RegExp) {
-      return api.test(url.href); // Check for regex match
-    }
-    return false // Not a match if it's not a string or regex
-  })
-}
 
 workbox.routing.registerRoute(
   ({ url }) => matchesApi(url),
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: 'api',
-    plugins: [
-      {
-        cacheWillUpdate: async ({ request, response }) => {
-          // Check if response is okay, if not return null to not cache it
-          if (!response || !response.ok) {
-            return null; // Return null to indicate not to cache
-          }
-          return response; // Return the valid response to cache it
-        },
+    plugins: [{
+      cacheWillUpdate: async ({ request, response }) => {
+        // Check if response is okay, if not return null to not cache it
+        if (!response || !response.ok) {
+          return null // Return null to indicate not to cache
+        }
+        return response // Return the valid response to cache it
       },
-    ],
+    }],
   })
 )
 
-// workbox.routing.registerRoute(
-//   ({ url }) => url.href === API_URL,
-//   new workbox.strategies.StaleWhileRevalidate({
-//     cacheName: 'api',
-//     plugins: [
-//       {
-//         cacheWillUpdate: async ({ request, response }) => {
-//           // Check if response is okay, if not return undefined to not cache it
-//           if (!response || response.status !== 200) {
-//             return null
-//           }
-//           return response // return the response to cache it
-//         },
-//       },
-//     ],
-//   })
-// )
-
-// Service Worker code to cache dynamic API responses
-// const API_URL_PATTERN = /^https:\/\/legitski\.com\/api\/protected\/store\/\d+$/; // match the pattern with a number at the end
-// workbox.routing.registerRoute(
-//   ({ url }) => API_URL_PATTERN.test(url.href),
-//   new workbox.strategies.StaleWhileRevalidate({
-//     cacheName: 'store-api-cache',
-//     plugins: [
-//       {
-//         cacheWillUpdate: async ({ request, response }) => {
-//           // Check if the response is okay; if not, return null to not cache it
-//           if (!response || response.status !== 200) {
-//             return null;
-//           }
-//           return response; // return the response to cache it 
-//         },
-//       },
-//     ],
-//   })
-// );
+function matchesApi(url) {
+  return getAPIs.some(api => {
+    if (typeof api === 'string') {
+      return api === url.href // Check for string match
+    } else if (api instanceof RegExp) {
+      return api.test(url.href) // Check for regex match
+    }
+    return false // Not a match if it's not a string or regex
+  })
+}
