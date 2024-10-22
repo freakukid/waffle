c2b95aed3415af8ba38f51c3c2ade937e1e832e4
omni-frontend-dev <spadilla@omnislash.com>
2024-10-22 15:35:40 -0700

    offline post requests

--- pages/offline.vue
+++ pages/offline.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="text-3xl pt-16 text-center">Offline Page</h1>
+    <h1 class="text-3xl pt-16 text-center">Offline Queue</h1>
 
     <div v-if="requests.length">
       <!-- LOGS -->
