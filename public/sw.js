74ca7b4d3ac9ffd9ac65c7771cca7d2ede05e3a5
omni-frontend-dev <spadilla@omnislash.com>
2024-11-06 14:52:49 -0700

    changing auth starts now

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js')
 
-const CACHE_NAME = 'static-v10'
+const CACHE_NAME = 'static-v11'
 
 self.addEventListener('install', (event) => {
   // Immediately take control, and skip waiting
